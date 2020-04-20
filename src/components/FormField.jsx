2f8990a54ba90ffee3810c7c764f14170e569e11
Gabriela <24256452+gabrielacarr@users.noreply.github.com>
2020-04-20 18:29:47 -0400

    placed = sign to line 9

--- src/components/FormField.jsx
+++ src/components/FormField.jsx
@@ -6,7 +6,7 @@ const formfield = (props) => {
         <div className="field">
         <label className="label">{props.label}</label>
         <div className="control">
-        <input className="input" type{props.type} placeholder={props.placeholder} />
+        <input className="input" type={props.type} placeholder={props.placeholder} />
         </div>
         </div>
     );
