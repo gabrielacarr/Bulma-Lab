769485398a4a216282372f893277d31715f454ba
Gabriela <24256452+gabrielacarr@users.noreply.github.com>
2020-04-21 07:38:35 -0400

    fat arrow

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,17 +1,34 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Message extends Component {
-  render() {
-    return (
-      <article class="message">
-        <div class="message-header">
-          <p>{this.props.title}</p>
-          <button class="delete" aria-label="delete"></button>
-        </div>
-        <div class="message-body">{this.props.children}</div>
-      </article>
-    );
-  }
+    render() {
+        return (
+            <article class="message">
+            <div class="message-header">
+            <p>{this.props.title}</p>
+            <button class="delete" aria-label="delete"></button>                
+            </div>
+            <div class="message-body">
+            {this.props.children}
+            </div>
+            </article>
+        );
+    }
 }
 
 export default Message;
+
+
+// const Message = (props) => {
+//     return (
+//       <div className="message">
+//         <label className="message-header">{props.label}</label>
+//         <div className="message-body">
+//         <button class="delete" aria-label="delete"></button> 
+//           />
+//         </div>
+//       </div>
+//     );
+//   };
+//   export default Message;
+  
